54549439cf4b60050090c91579032972dd0a851b
Saheed Collins <srasaq00@gmail.com>
2023-06-08 14:11:03 -0500

    d

--- firebase cdn/index.js
+++ firebase cdn/index.js
@@ -33,7 +33,7 @@ let blog = []
 
 
 
-// let display = document.getElementById('display')
+let display = document.getElementById('displayEachPost')
 onSnapshot(colRef, (collins)=>{
   
   // display.innerHTML= ''
@@ -42,26 +42,25 @@ onSnapshot(colRef, (collins)=>{
     //   console.log(sam.data(), );
       display.innerHTML+=
       `
+    <div class=" grid-container gap-4 d-flex flex-wrap">
+          <div class=" col-lg-4 col-md-4 col-12 bg-info w-100 ">
 
-      <div class=" col-lg-4 col-md-4 col-12 bg-info w-100 ">
+          <div class="box">
+          <!-- input image -->
+          <img src="${sam.data().file}" alt="">
+          </div>
 
-      <div class="box">
-      <!-- input image -->
-      <img src="${sam.data().file}" alt="">
-  </div>
+          <h6 class="mt-2 smallHeading">${sam.data().tagline} </h6>
 
-                    <h6 class="mt-2 smallHeading">
-                        ${sam.data().tagline}
-                        </h6>
+          <a href="./EducationPagePost.html"> <h3 class="pt-1">${sam.data().title}</h3></a>
 
-                   <a href="./EducationPagePost.html"> <h3 class="pt-1">
-                    ${sam.data().title}
-                   </h3></a>
+          <p> ${sam.data().blogDescrip}</p>
 
-                    <p> ${sam.data().blogDescrip}</p>
+          <button class="p-2 px-4 Hoverbtn"> <a href="./EducationPagePost.html"> View Posts</a>
+          </button>
 
-                    <button class="p-2 px-4 Hoverbtn"> <a href="./EducationPagePost.html"> View Posts</a></button>
-                </div>
+          </div>
+      </div> <br>
 `
   })
 })
@@ -123,68 +122,22 @@ getDoc(oneDocRef)
 
 
 
+
 // const display = document.querySelector("#display_post")
-let displayPost = document.getElementById("display_post")
+
 let menuOpt = document.getElementById('menuOpt')
 let menuOpt2 = document.getElementById('menuOpt2')
 let menuOpt3 = document.getElementById('menuOpt3')
 let menuOpt4 = document.getElementById('menuOpt4')
 
 
-function addBlog_Post() {
+function scrollTopButt() {
   menuOpt2.style.backgroundColor = "#ee4400";
   menuOpt3.style.backgroundColor = "";
   menuOpt4.style.backgroundColor = "";
   menuOpt.style.backgroundColor = ""; 
 
-  displayPost.innerHTML =
-  `
-  <div class="pagePost_toPush col-12 col-md-9 col-lg-10 p-4" id="pagePost_toPush">
 
-      <div class="leftsect p-3" id="leftsect">
-        
-        <h2 class="welcome">New Posts</h2>
-        <hr>
-        
-  <div class=" p-1">
-  
-    <div class="row ">
-         <div class="col-12 col-md-3 col-lg-3"> 
-          <h6 class="">Tagline</h6>
-          <input type="text" name="tagline" id="tagline">
-        </div>
-  
-        <div class="col-12 col-md-3 col-lg-3"> 
-          <h6 class="">Topic</h6>
-          <input type="text" name="topic" id="topic">
-          
-        </div>
-        <div class="col-12 col-md-3 col-lg-6"> 
-          <h6 class="">Little Intro</h6>
-          <textarea type="text" name="into" id="intro" spellcheck="true"> </textarea>
-        </div>
-  </div>
-        <br>
-       
-        <div class=""> 
-          <h6 class="">Upload Picture</h6>
-         <input type="file" name="Chose Image" id="">
-        </div>
-  
-        <br>
-        <div class=""> 
-          <h6 class="">Body</h6>
-          <textarea type="text" name="into" id="body" spellcheck="true"> </textarea>
-        </div>
-  
-        <br>
-        <button class="p-2 px-4 Hoverbtn"><a href=""> Publish</a></button> 
-        </div>
-  
-      </div>
-  
-    </div>
-  `
 }
 
 function readersPost() {
@@ -193,13 +146,11 @@ function readersPost() {
   menuOpt4.style.backgroundColor = "";
   menuOpt.style.backgroundColor = "";
 
-  // let wholePage =  displayPost.innerHTML ;
 
-  // wholePage.stlye.display = "none";
 }
 
-let displayDashBud = document.getElementById("displayDashBud")
-let dashBud = document.getElementById("dashBud").innerHTML
+// let displayDashBud = document.getElementById("displayDashBud")
+// let dashBud = document.getElementById("dashBud").innerHTML
 
 function logOutAdmin() {
 dashBud.style.dislpay = "none"
